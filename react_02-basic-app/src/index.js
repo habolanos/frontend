d22c6c83226c144d823ebeb822d2e9d1bc8448fc
Harold Adrian Bolanos Rodriguez <haroldadrian@gmail.com>
2021-05-19 23:31:48 -0500

    Montaje de aplicaciones React

---
+++ react_02-basic-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//Componentes Creados
+import ComponenteApp from './ComponenteApp'
+
+//Hojas de Estilos
+import './index.css'
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(<ComponenteApp />, divRoot);
